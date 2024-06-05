224cc6818fee244d63fc2a245fcfe5b04d8f62b9
Yash Singhal <csdsa772023@gmail.com>
2024-06-05 20:51:58 +0530

    Add files via upload
    Update AM or PM

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
 let hrs=document.getElementById("hrs");
 let min=document.getElementById("min");
 let sec=document.getElementById("sec");
+let MorningNight=document.getElementById("MorningNight");
+
 
 
 setInterval(() => {
@@ -9,7 +11,24 @@ setInterval(() => {
 // console.log(currentTime.getHours());
 // console.log(currentTime.getMinutes());
 
+// let ampm=hrs >=12 ?"PM":"AM";
+
 hrs.innerHTML=(currentTime.getHours() < 10 ?"0":"") + currentTime.getHours();//So that it start from 01,02,03,04to,09
 min.innerHTML=(currentTime.getMinutes() < 10 ?"0":"") + currentTime.getMinutes();//So that it start from 01,02,03,04to,09
 sec.innerHTML=(currentTime.getSeconds() < 10 ?"0":"") + currentTime.getSeconds();//So that it start from 01,02,03,04to,09
+
+if(hrs >=12)
+{
+    MorningNight.innerHTML="AM"
+}
+else{
+    MorningNight.innerHTML="PM"
+}
+
+if(hrs > 12){
+    hrs=hrs-12;
+    console.log(hrs);
+}
+
+
 }, 1000);
